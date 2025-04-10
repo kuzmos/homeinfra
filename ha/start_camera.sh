bc3912f8a153f4243dc9178b997c37d151e70a99
admin <alexander.kuzmin@gmail.com>
2025-04-10 20:44:58 +0200

    Tuned quality image

--- ha/start_camera.sh
+++ ha/start_camera.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    rpicam-vid -t 0 --inline --codec h264 --width 1920 --height 1080 --bitrate 10000000 --listen -o tcp://0.0.0.0:8000
+    rpicam-vid -t 0 --inline --codec mjpeg --quality 95 --width 2592 --height 1944 --framerate 7 --listen -o tcp://0.0.0.0:8000 # 45% CPU
     echo "Camera crashed, restarting..."
     sleep 1
 done
