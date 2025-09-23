04a2a3f5e2b042bef0dd0d28725a3d510e116b35
admin <alexander.kuzmin@gmail.com>
2025-09-23 10:29:24 +0200

    Postfix after mailbox creation script debugged

--- mail/postfix_after.sh
+++ mail/postfix_after.sh
@@ -1,7 +1,7 @@
 #!/bin/ksh
 
 if [[ $# -ne 4 ]]; then
-	echo "Expected 3 parameters: <new_mail_username> <domain> <root_maildir> <quota>"
+	echo "Expected 4 parameters: <new_mail_username> <domain> <root_maildir> <quota>"
 	exit 1
 fi
 
@@ -9,31 +9,42 @@ new_username="$1"
 domain="$2"
 root_maildir="$3"
 mail_group="mailers"
+mail_base_dir="/var/mail/vhosts"
+vmail_user="vmail"
+newdirbase="${mail_base_dir}/${root_maildir}"
 
-mkdir "${root_maildir}"/"${new_username}"/sieve
+mkdir "${newdirbase}"
+
+if [[ $? -ne 0 ]]; then
+	echo "creating new base mail dir failed"
+	exit 1
+fi
+
+
+mkdir "${newdirbase}"/sieve
 
 if [[ $? -ne 0 ]]; then
 	echo "creating sieve dir failed"
 	exit 1
 fi
 
-touch "${root_maildir}"/"${new_username}"/sieve/managesieve.sieve
+touch "${newdirbase}"/sieve/managesieve.sieve
 
 if [[ $? -ne 0 ]]; then
 	echo "creating managesieve.sieve file failed"
 	exit 1
 fi
 
-ln -s "${root_maildir}"/"${new_username}"/sieve/managesieve.sieve "${root_maildir}"/"${new_username}"/.dovecot.sieve
+ln -s "${newdirbase}"/sieve/managesieve.sieve "${newdirbase}"/.dovecot.sieve
 
 if [[ $? -ne 0 ]]; then
-	        echo 'creating symlink "${root_maildir}"/"${new_username}"/.dovecot.sieve to "${root_maildir}"/"${new_username}"/sieve/managesieve.sieve failed'
+	        echo 'creating symlink "${newdirbase}"/.dovecot.sieve to "${newdirbase}"/sieve/managesieve.sieve failed'
 		        exit 1
 fi
 
-chown -h "${new_username}":"${mail_group}" "${root_maildir}"/"${new_username}"/.dovecot.sieve
+chown -h "${vmail_user}":"${mail_group}" "${newdirbase}"/.dovecot.sieve
 
 if [[ $? -ne 0 ]]; then
-	        echo 'changing symlink "${root_maildir}"/"${new_username}"/.dovecot.sieve owner to "${new_username}" failed'
+	        echo 'changing symlink "${newdirbase}"/.dovecot.sieve owner to "${vmail_user}i:${mail_group}" failed'
 		        exit 1
 fi
