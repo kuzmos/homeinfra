e1c0e2a8df18df0bdbc084f58640b901c7d1987f
admin <alexander.kuzmin@gmail.com>
2025-09-23 10:32:24 +0200

    Postfix after mailbox creation script debugged #3

--- mail/postfix_after.sh
+++ mail/postfix_after.sh
@@ -38,13 +38,13 @@ fi
 ln -s "${newdirbase}"/sieve/managesieve.sieve "${newdirbase}"/.dovecot.sieve
 
 if [[ $? -ne 0 ]]; then
-	        echo 'creating symlink "${newdirbase}"/.dovecot.sieve to "${newdirbase}"/sieve/managesieve.sieve failed'
+	        echo 'creating symlink "${newdirbase}"/.dovecot.sieve to "${newdirbase}"sieve/managesieve.sieve failed'
 		        exit 1
 fi
 
 chown -h "${vmail_user}":"${mail_group}" "${newdirbase}"/.dovecot.sieve
 
 if [[ $? -ne 0 ]]; then
-	        echo 'changing symlink "${newdirbase}"/.dovecot.sieve owner to "${vmail_user}i:${mail_group}" failed'
+	        echo 'changing symlink "${newdirbase}"/.dovecot.sieve owner to "${vmail_user}:${mail_group}" failed'
 		        exit 1
 fi
